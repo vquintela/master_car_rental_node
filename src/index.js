d175dee5b1e226f3684ca48ea6abf5f1b72602a5
vitor <vitor@vitor>
2020-04-27 21:30:31 -0300

    index

--- src/index.js
+++ src/index.js
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
 });
 
 //routes
+app.use(require('./routes/index'))
 app.use(require('./routes/autenticacion'));
 app.use('/users', require('./routes/users'));
 app.use('/automoviles', require('./routes/automoviles'));
